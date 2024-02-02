49a8a22e024c4444449861571ac6f70f9618494d
Americo509 <americorosagustavo@hotmail.com>
2024-02-02 12:24:01 -0300

    AudioBook

---
+++ main.js
@@ -0,0 +1,65 @@
+const botaoPlayPause = document.getElementById('play-pause');
+const botaoAvancar = document.getElementById('proximo');
+const botaoVoltar = document.getElementById('anterior');
+const audioCapitulo = document.getElementById('audio-capitulo');
+const nomeCapitulo = document.getElementById('capitulo');
+
+const numeroCapitulos = 10;
+let taTocando = 0;
+let capituloAtual = 1;
+
+function tocarFaixa() {
+    audioCapitulo.play();
+    botaoPlayPause.classList.remove('bi-play-circle-fill');
+    botaoPlayPause.classList.add('bi-pause-circle-fill');
+}
+
+function pausarFaixa() {
+    audioCapitulo.pause()
+    botaoPlayPause.classList.remove('bi-pause-circle-fill');
+    botaoPlayPause.classList.add('bi-play-circle-fill');
+}
+
+function tocarOuPausar() {
+    if (taTocando === 0) {
+        tocarFaixa();
+        taTocando = 1;
+    } else if (taTocando === 1){
+        pausarFaixa();
+        taTocando = 0;
+    }
+}
+
+function trocarNomeCapitulo() {
+    nomeCapitulo.innerText = 'Capítulo ' + capituloAtual;
+}
+
+function proximaFaixa() {
+    if (capituloAtual === numeroCapitulos) {
+        capituloAtual = 1;
+    } else {
+        capituloAtual = capituloAtual + 1;
+    }
+
+    audioCapitulo.src = 'books/dom-casmurro/' + capituloAtual + '.mp3';
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeCapitulo();
+}
+
+function voltarFaixa() {
+    if (capituloAtual === 1) {
+        capituloAtual = numeroCapitulos;
+    } else {
+        capituloAtual = capituloAtual - 1;
+    }
+
+    audioCapitulo.src = 'books/dom-casmurro/' + capituloAtual + '.mp3';
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeCapitulo();
+}
+
+botaoPlayPause.addEventListener('click', tocarOuPausar);
+botaoAvancar.addEventListener('click', proximaFaixa);
+botaoVoltar.addEventListener('click', voltarFaixa);
